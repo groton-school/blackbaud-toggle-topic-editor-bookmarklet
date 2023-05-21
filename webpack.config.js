ba38cd64c6fc41c84969f4292eb0526066706fff
Seth Battis <sbattis@groton.org>
2023-05-21 15:47:01 -0400

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-const config = require('@battis/webpack/ts/bookmarklet');
-
-module.exports = config({
+module.exports = require('@battis/webpack/ts/bookmarklet')({
   root: __dirname,
   title: 'Toggle Topic Editor',
   package: require('./package.json')
